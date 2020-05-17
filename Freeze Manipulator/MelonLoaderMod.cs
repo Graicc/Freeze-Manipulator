e4131f4948eae09cbca1cd672a96b74c2cc2dd81
Graicc <33105645+Graicc@users.noreply.github.com>
2020-05-17 01:36:35 -0400

    v1.0.0

---
+++ Freeze Manipulator/MelonLoaderMod.cs
@@ -0,0 +1,57 @@
+﻿using MelonLoader;
+using StressLevelZero;
+using BoneworksModdingToolkit;
+using UnityEngine;
+using StressLevelZero.Props.Weapons;
+using Il2CppSystem.Runtime.Remoting.Messaging;
+using StressLevelZero.Interaction;
+using UnityEngine.Experimental.PlayerLoop;
+using Il2CppSystem.Collections.Generic;
+
+namespace Freeze_Manipulator
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Freeze_Manipulator"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Graic"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class Freeze_Manipulator : MelonMod
+    {
+        Stack<Rigidbody> frozen = new Stack<Rigidbody>();
+
+        float doublePressTimer = 0;
+        const float doublePressTimerLenght = 0.5f;
+
+        public override void OnUpdate() {
+            if (!Player.FindPlayer()) return;
+            UpdateFreeze(Player.rightHand);
+            UpdateFreeze(Player.leftHand);
+            doublePressTimer = doublePressTimer > 0 ? doublePressTimer -= Time.deltaTime : 0;
+        }
+
+        void UpdateFreeze(Hand hand) {
+            GameObject go = Player.GetObjectInHand(hand);
+            if (go) {
+                DevManipulatorGun dmg = go.GetComponentInParent<DevManipulatorGun>();
+                if (dmg && hand.controller.GetBButtonDown()) {
+                    if (dmg.m_IsGrabbed) { // Freeze
+                        dmg.m_GrabbedRigidbody.isKinematic = true;
+                        frozen.Push(dmg.m_GrabbedRigidbody);
+                        dmg.Blast();
+                        dmg.gravityManipulator.ReleaseLockedRigidbody();
+                    } else if (doublePressTimer > 0 && frozen.Count > 0) { // Unfreeze
+                        frozen.Pop().isKinematic = false;
+                        dmg.Blast();
+                        doublePressTimer = 0;
+                    } else {
+                        doublePressTimer = doublePressTimerLenght;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Freeze Manipulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(Freeze_Manipulator.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Freeze_Manipulator.BuildInfo.Company)]
+[assembly: AssemblyProduct(Freeze_Manipulator.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Freeze_Manipulator.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Freeze_Manipulator.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(Freeze_Manipulator.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Freeze_Manipulator.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(Freeze_Manipulator.Freeze_Manipulator), Freeze_Manipulator.BuildInfo.Name, Freeze_Manipulator.BuildInfo.Version, Freeze_Manipulator.BuildInfo.Author, Freeze_Manipulator.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame(null, null)]
